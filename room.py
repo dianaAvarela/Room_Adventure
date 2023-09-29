a3de8acc07b4502a542c74e745552d985e1019ae
Diana <dianasaursrex8520@gmail.com>
2023-09-29 15:14:09 -0500

    first commit

---
+++ room.py
@@ -0,0 +1,50 @@
+
+
+class Room:
+
+    def __init__(self, name: str, image_filepath: str):
+        """""
+            A Room in the Room Adventure game.
+
+            name: str - a name for the room
+            image_filepath: str - the relative filepath to the image
+                ex: images/room1.gif 
+        """""
+        self.name = name
+        self.image = image_filepath
+        self.exits = {}
+        self.items = {}
+        self.grabbables = {}
+
+    #additional methods
+    def add_exits(self, location: str, room: 'Room | None'):
+        """"
+            Adds and exit to the room.
+
+            location: str - a direction that identifies the location of the room you will go to
+            room: Room | None - a room object or None that the location leads to 
+                In case if the "death sequence", use None instead of a Room object.
+        """
+        self.exits[location] = room
+
+    def add_item(self, label: str, description: str) -> None:
+        self.items[label] = description
+
+    def add_grabbable(self, item: str):
+        self.grabbables.append(item)
+
+    def delete_grabbable(self, item: str):
+        self.grabbables.remove(item)
+
+    def __str__(self) -> str:
+        result = f"You are in {self.name}"
+
+        result += "You see: "
+        for item in self.items.keys():
+            result += item + " "
+        result += "\n"
+        for exit in self.exits.keys():
+            result += exit + " "
+        result += "\n"
+
+        return result
